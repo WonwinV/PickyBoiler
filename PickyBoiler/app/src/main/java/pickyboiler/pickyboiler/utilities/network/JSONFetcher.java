584737f7b768baa37b90f5bb1be27e6a256b82e2
WonwinV <wonwinv@hotmail.com>
2018-02-11 16:44:07 -0500

    Add utility class to async fetch JSON
    override processFinish(String) in class using this JSONFetcher

---
+++ PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/utilities/network/JSONFetcher.java
@@ -0,0 +1,84 @@
+package pickyboiler.pickyboiler.utilities.network;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.content.ContentValues.TAG;
+
+public class JSONFetcher extends AsyncTask<String, String, String> {
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+    public AsyncResponse delegate = null;
+    public JSONFetcher(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        delegate.processFinish(result);
+        Log.d(TAG, "onPostExecute: " + result);
+    }
+
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        if(params == null) {
+            //GG
+            return null;
+        }
+
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            InputStream stream = connection.getInputStream();
+
+            reader = new BufferedReader(new InputStreamReader(stream));
+
+            StringBuffer buffer = new StringBuffer();
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+            return buffer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /*
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Log.d(TAG, "onPostExecute: " + result);
+    }*/
+}
+
