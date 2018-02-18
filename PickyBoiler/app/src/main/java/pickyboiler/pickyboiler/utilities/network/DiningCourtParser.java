f09030ed9434fc2e1fd456b7130b961e9adf3260
WonwinV <wonwinv@hotmail.com>
2018-02-17 22:03:01 -0500

    update Parser Documentation + fix bug
    update parser documentation + fix bug

--- PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/utilities/network/DiningCourtParser.java
+++ PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/utilities/network/DiningCourtParser.java
@@ -16,9 +16,11 @@ import java.util.Map;
 import static android.content.ContentValues.TAG;
 
 public class DiningCourtParser {
-    /*
+    /**
+     * superseded by parseDiningCourtJSON
      * return -1 if not open, 0 if no veggie menu
      */
+    @Deprecated
     public HashMap<String, Integer> countVegetarianMenu(JSONObject diningCourtJSON) {
         HashMap<String, Integer> counts = new HashMap<>();
         try {
@@ -50,7 +52,41 @@ public class DiningCourtParser {
 
     /**
      * @param diningCourtJSON
-     * @return JSONObject with "Location", "Date", and "MealDetails". Each "MealDetails" has "Status", "VegetarianCount", "AllergenCount" JSONObject, 'StartTime", "endTime"
+     * @return JSONObject with "Location", "Date", and "MealDetails".
+     *         Each "MealDetails" object has "Breakfast/Luch/.." key mapped to "Status", "VegetarianCount", "AllergenCount" JSONObject, 'StartTime", "endTime"
+     *         Each AllergenCount object has "Nut/Shrimp/..." key mapped to count of that allergens
+     *
+     *         Ex:
+     *         {
+     *             Location : Earhart
+     *             Date : 10/19/2017
+     *             MealDetails : {
+     *                 Breakfast : {
+     *                     Status : Open
+     *                     AllMeal : {
+     *                         Steak,
+     *                         ...
+     *                     }
+     *                     VegetarianCount : 3,
+     *                     AllergenCount : {
+     *                         Gluten : 2,
+     *                         Eggs : 3,
+     *                         ...
+     *                     }
+     *                     StartTime : 06:30:00
+     *                     EndTime : 10:00:00
+     *                     URL : https://...
+     *                 }
+     *
+     *                 Lunch : {
+     *                      ....
+     *                 }
+     *
+     *                 Dinner : {
+     *                     ....
+     *                 }
+     *             }
+     *         }
      */
     public JSONObject parseDiningCourtJSON(JSONObject diningCourtJSON) {
         JSONObject result = new JSONObject();
@@ -58,56 +94,76 @@ public class DiningCourtParser {
             Log.d("--------", "counting for " + diningCourtJSON.getString("Location"));
             result.put("Location", diningCourtJSON.getString("Location"));
             result.put("Date", diningCourtJSON.getString("Date"));
+            JSONObject allMealDetails = new JSONObject();
+
             for (int i = 0; i < diningCourtJSON.getJSONArray("Meals").length(); i++) {
-                JSONObject mealObject = new JSONObject();
-                int counter = 0;
-                JSONObject meal = diningCourtJSON.getJSONArray("Meals").getJSONObject(i);
+                //details of each meal
                 JSONObject mealDetail = new JSONObject();
+                int counter = 0;
+                //all meal objects
+                JSONObject meal = diningCourtJSON.getJSONArray("Meals").getJSONObject(i);
 
                 if(meal.getString("Status").equals("Open")) {
                     Map<String, Integer> countAllergens = new HashMap<>();
                     JSONArray allStations = meal.getJSONArray("Stations");
+                    JSONObject allMenu = new JSONObject();
+
                     for (int j = 0; j < allStations.length(); j++) {
                         JSONArray allItems = allStations.getJSONObject(j).getJSONArray("Items");
                         for (int k = 0; k < allItems.length(); k++) {
+                            //JSONArray of present allergens for a single item
+                            JSONArray menuAllergens = new JSONArray();
+
+                            //count Veggies
                             String isVeggie = allItems.getJSONObject(k).getString("IsVegetarian");
                             if (isVeggie != null && isVeggie.equals("true")) {
+                                //menuAllergens.put("isVegeterian");
                                 counter++;
                             }
-                            JSONArray allAllergens = null;
+                            //count Allergies
+                            JSONArray allAllergens;
                             try {
                                 allAllergens = allItems.getJSONObject(k).getJSONArray("Allergens");
                             } catch (JSONException e) {
                                 allAllergens = null;
                             }
+
                             if (allAllergens != null) {
                                 for (int l = 0; l < allAllergens.length(); l++) {
                                     JSONObject obj = allAllergens.getJSONObject(l);
                                     if(obj.getBoolean("Value")) {
+                                        menuAllergens.put(obj.getString("Name"));
                                         if (countAllergens.containsKey(obj.getString("Name"))) {
                                             countAllergens.put(obj.getString("Name"), ((Integer) countAllergens.get(obj.getString("Name"))) + 1);
                                         } else {
-                                            countAllergens.put(obj.getString("Name"), 0);
+                                            countAllergens.put(obj.getString("Name"), 1);
                                         }
                                     }
                                 }
                             }
+
+                            //list of all menu items, each item with name and allergens list
+                            allMenu.put(allItems.getJSONObject(k).getString("Name"), menuAllergens);
+                            //Log.d("....menuAllergen", ">" + allItems.getJSONObject(k).getString("Name") + " : " + menuAllergens.toString());
                         }
                     }
                     Log.d("****" + meal.getString("Name") + " @ " + diningCourtJSON.getString("Location") + " VegieCount", "parseDiningCourtJSON: " + counter);
                     Log.d("****" + meal.getString("Name") + " @ " + diningCourtJSON.getString("Location") + " AllergenMap", "parseDiningCourtJSON: " + countAllergens.toString());
+                    mealDetail.put("Location", diningCourtJSON.getString("Location"));
                     mealDetail.put("Status", meal.getString("Status"));
+                    mealDetail.put("AllMeal", allMenu);
                     mealDetail.put("VegetarianCount", counter);
                     mealDetail.put("AllergenCount", new JSONObject(countAllergens));
                     mealDetail.put("StartTime", meal.getJSONObject("Hours").getString("StartTime"));
                     mealDetail.put("EndTime", meal.getJSONObject("Hours").getString("EndTime"));
-                    mealObject.put(meal.getString("Name"), mealDetail);
-                    result.put("MealDetails", mealObject);
+                    mealDetail.put("URL", "https://dining.purdue.edu/menus/" + diningCourtJSON.getString("Location") + "/");
+                    allMealDetails.put(meal.getString("Name"), mealDetail);
                 } else {
                     mealDetail.put("Status", meal.getString("Status"));
                     result.put(meal.getString("Name"), mealDetail);
                 }
             }
+            result.put("MealDetails", allMealDetails);
         } catch (JSONException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -116,4 +172,5 @@ public class DiningCourtParser {
         }
         return result;
     }
+    
 }
