f246172364cf35dd278207e731c658a4a1b5df28
Jackson Sorrells <sorrelj@purdue.edu>
2018-02-22 18:27:48 -0500

    Finished OnClick handler method

--- PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/AllergenTabFragment.java
+++ PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/AllergenTabFragment.java
@@ -9,8 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ToggleButton;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
+
+import android.widget.Toast;
 
 import pickyboiler.pickyboiler.utilities.storage.SharedPreferencesManager;
 
@@ -89,7 +93,159 @@ public class AllergenTabFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
+          boolean pressed = ((ToggleButton) view).isChecked();
+          CharSequence text = "";
+          Drawable drawable;
 
+          switch (view.getId()) {
+
+              case R.id.egg_btn:
+                  if (pressed) {
+                      text = "Added egg to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.egg);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenEgg), "true");
+                  }
+                  else {
+                      text = "Removed egg from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.egg_bw);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenEgg), "false");
+                      view.setBackgroundDrawable(drawable);
+                  }
+                  break;
+
+
+              case R.id.fish_btn:
+                  if (pressed) {
+                      text = "Added fish to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.fish);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenFish), "true");
+                  }
+                  else {
+                      text = "Removed fish from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.fish_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenFish), "false");
+                  }
+                  break;
+
+
+              case R.id.gluten_btn:
+                  if (pressed) {
+                      text = "Added gluten to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.gluten);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenGluten), "true");
+                  }
+                  else {
+                      text = "Removed gluten from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.gluten_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenGluten), "false");
+                  }
+                  break;
+
+
+              case R.id.milk_btn:
+                  if (pressed) {
+                      text = "Added milk to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.milk);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenMilk), "true");
+                  }
+                  else {
+                      text = "Removed milk from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.milk_bw);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenMilk), "false");
+                      view.setBackgroundDrawable(drawable);
+                  }
+                  break;
+
+
+              case R.id.nut_btn:
+                  if (pressed) {
+                      text = "Added nut to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.nut);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenNut), "true");
+                  }
+                  else {
+                      text = "Removed nut from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.nut_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenNut), "false");
+                  }
+                  break;
+
+
+              case R.id.peanut_btn:
+                  if (pressed) {
+                      text = "Added peanut to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.peanut);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenPeanut), "true");
+                  }
+                  else {
+                      text = "Removed peanut from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.peanut_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenPeanut), "false");
+                  }
+                  break;
+
+
+              case R.id.shellfish_btn:
+                  if (pressed) {
+                      text = "Added shellfish to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.shellfish);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenShellfish), "true");
+                  }
+                  else {
+                      text = "Removed shellfish from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.shellfish_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenShellfish), "false");
+                  }
+                  break;
+
+
+              case R.id.soy_btn:
+                  if (pressed) {
+                      text = "Added soy to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.soy);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenSoy), "true");
+                  }
+                  else {
+                      text = "Removed soy from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.soy_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenSoy), "false");
+                  }
+                  break;
+
+
+              case R.id.wheat_btn:
+                  if (pressed) {
+                      text = "Added wheat to allergens.";
+                      drawable = getResources().getDrawable(R.drawable.wheat);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenWheat), "true");
+                  }
+                  else {
+                      text = "Removed wheat from allergens.";
+                      drawable = getResources().getDrawable(R.drawable.wheat_bw);
+                      view.setBackgroundDrawable(drawable);
+                      SharedPreferencesManager.putStringSharedPreferences(getString(R.string.AllergenWheat), "false");
+                  }
+                  break;
+
+          }
+
+          
+          SharedPreferencesManager.showToast((String) text);
 
         }
 
