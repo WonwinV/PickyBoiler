6cd879452e1a8ceb9b42e67adea45d546bcabb94
coreyc723 <36377124+coreyc723@users.noreply.github.com>
2018-03-27 14:22:32 -0400

    Update MenuDisplayPage.java
    makes menu items clickable, so that clicked item may be added to favorites

--- PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/MenuDisplayPage.java
+++ PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/MenuDisplayPage.java
@@ -1,20 +1,33 @@
 package pickyboiler.pickyboiler;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
-import android.view.MenuItem;
-import android.widget.EditText;
+import android.text.Spanned;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.text.SpannableString;
+import android.text.style.ClickableSpan;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.widget.PopupWindow;
+import android.widget.Button;
+import android.text.method.LinkMovementMethod;
+import android.support.v4.app.Fragment;
+import android.text.TextPaint;
 
+import java.io.Console;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import pickyboiler.pickyboiler.Utilities.Adapters.DiningCourtAdapter;
+import pickyboiler.pickyboiler.Utilities.Storage.SharedPreferencesManager;
+
 
 /**
  * Created by Jackson Sorrells on 3/2/2018.
@@ -24,9 +37,17 @@ public class MenuDisplayPage extends AppCompatActivity {
 
     public String diningCourtName;
     public ArrayList<String> menuItems;
+    String menuFinal;
+    int ini;
+    int fin;
+    ArrayList<String> favItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //retrieve favorites list for potential updating
+        favItemsList = SharedPreferencesManager.getPrefFavListtFofy();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
@@ -37,8 +58,8 @@ public class MenuDisplayPage extends AppCompatActivity {
 
         ImageView menuIMG = (ImageView) findViewById(R.id.menuimg);
         TextView menuText = (TextView) findViewById(R.id.MenuText);
+        menuText.setMovementMethod(LinkMovementMethod.getInstance());
 
-        menuText.setMovementMethod(new ScrollingMovementMethod());
 
         int menuID = R.drawable.default_menu_logo;
         switch (diningCourtName){
@@ -62,17 +83,106 @@ public class MenuDisplayPage extends AppCompatActivity {
         menuIMG.setImageDrawable(getResources().getDrawable(menuID));
 
 
-        String menuFinal = "";
+        menuFinal = "";
         for (int i = 0; i < menuItems.size(); i++){
             menuFinal += menuItems.get(i) + "\n";
         }
 
-        menuText.setText(menuFinal);
+        //define spannable string and clickable span functionality
+        SpannableString ss = new SpannableString(menuFinal);
 
-        /* will add code shortly */
+        //mechanism for making each line of span clickable
+        int currPos;
+        for(currPos = 0; currPos < menuFinal.length(); currPos++) {
+            ini = currPos;
+            while(menuFinal.charAt(currPos + 1) !=  '\n') {
+                if(currPos >= menuFinal.length()) {
+                    break;
+                }
+                currPos++;
+            }
+            fin = currPos;
 
+            //define new clickable item
+            ClickableSpan newclickableSpan = new ClickableSpan() {
+                int curr1 = ini;
+                int curr2 = fin;
+                @Override
+                public void onClick(View view) {
+                    showPopup(MenuDisplayPage.this, curr1, curr2);
+                }
+
+                @Override
+                public void updateDrawState(TextPaint ds) {
+                    ds.setUnderlineText(false);
+                }
+            };
+
+            //add to span
+            ss.setSpan(newclickableSpan, ini, fin, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            //to newline; increment puts us on new line past the newline char
+            currPos++;
+        }
+        menuText.setText(ss);
     }
 
 
+    //method to display popup
+    private void showPopup(final Activity context, int first, int last) {
+        int width = 1000;
+        int height = 950;
+        final int fir = first;
+        final int las = last;
 
+        //inflate popup_layout
+        LinearLayout viewGroup= (LinearLayout) context.findViewById(R.id.popup);
+        LayoutInflater layoutInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = layoutInflater.inflate(R.layout.menu_popup, viewGroup);
+
+        //create the popup
+        final PopupWindow popup = new PopupWindow(context);
+        popup.setContentView(layout);
+        popup.setWidth(width);
+        popup.setHeight(height);
+        popup.setFocusable(true);
+
+        //clear default background
+        popup.setBackgroundDrawable(new BitmapDrawable());
+
+
+        //popup appears at center screen
+        popup.showAtLocation(layout, Gravity.CENTER, 0,0);
+
+        // Getting a reference to left button, and close the popup when clicked.
+        Button right = (Button) layout.findViewById(R.id.lbutton);
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String favItem = menuFinal.substring(fir,las+1);
+                if (favItemsList.contains(favItem.trim())) {
+                    SharedPreferencesManager.showToast("Item already added.");
+                    popup.dismiss();
+                    return;
+                }
+                SharedPreferencesManager.addFavoriteItem(getApplicationContext(), favItem.trim());
+                favItemsList.add(favItem.trim());
+                SharedPreferencesManager.showToast("Item added.");
+                Collections.sort(favItemsList);
+                popup.dismiss();
+            }
+        });
+
+        // Getting a reference to right button, and close the popup when clicked.
+        Button close = (Button) layout.findViewById(R.id.rbutton);
+        close.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                popup.dismiss();
+            }
+        });
+
+    }
 }
