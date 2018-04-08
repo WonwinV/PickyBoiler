e12838cf50ae2fdffb68508994d0d156c0f7b872
stichach <stichach@purdue.edu>
2018-04-08 19:47:07 -0400

    Polishing up the layouts

--- PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/OffCampusDining.java
+++ PickyBoiler/app/src/main/java/pickyboiler/pickyboiler/OffCampusDining.java
@@ -76,7 +76,7 @@ public class OffCampusDining extends AppCompatActivity
         tabLayout.setSelectedTabIndicatorColor(Color.parseColor("#FFB52B"));
         tabLayout.setSelectedTabIndicatorHeight((int) (5 * getResources().getDisplayMetrics().density));
         tabLayout.setTabTextColors(Color.parseColor("#727272"), Color.parseColor("#FFB52B"));
-
+        tabLayout.setBackgroundColor(Color.parseColor("#F2F2F2"));
 
 
 
